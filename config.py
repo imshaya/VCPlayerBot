9e4f5e57cfe83b22925977088db48477630b9a41
impta <87300042+impta@users.noreply.github.com>
2022-05-11 16:50:40 +0430

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ except ModuleNotFoundError:
 class Config:
     #Telegram API Stuffs
     load_dotenv()  # load enviroment variables from .env file
-    ADMIN = "ADMINS", ''
+    ADMIN = "ADMINS"
     API_ID = int("11042778")
     API_HASH = "API_HASH"
     BOT_TOKEN = "BOT_TOKEN"
