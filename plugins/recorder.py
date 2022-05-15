6f0c263356d32ec2fc8de02c3465d725192fea79
impta <87300042+impta@users.noreply.github.com>
2022-05-16 03:11:10 +0430

    Update recorder.py

--- plugins/recorder.py
+++ plugins/recorder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Copyright (C) @subinps
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -37,23 +37,23 @@ admin_filter=filters.create(is_admin)
 
 @Client.on_message(filters.command(["record", f"record@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
 async def record_vc(bot, message):
-    await message.reply("Configure you VCPlayer Recording settings from hereㅤㅤ ㅤ", reply_markup=(await recorder_settings()))
+    await message.reply("تنظیمات ضبط کنندهㅤㅤ ㅤ", reply_markup=(await recorder_settings()))
     await delete_messages([message])
 
 @Client.on_message(filters.command(["rtitle", f"rtitle@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
 async def recording_title(bot, message):
-    m=await message.reply("Checking..")
+    m=await message.reply("بررسی..")
     if " " in message.text:
         cmd, title = message.text.split(" ", 1)
     else:
-        await m.edit("Give me a new title. Use /rtitle < Custom Title >\nUse <code>False</code> to revert to default title")
+        await m.edit("بهم یه عنوان جدید بده، از /rtitle < Custom Title >\nUse <code>False</code> استفاده کن برای تبدیل ب عنوان اصلی")
         await delete_messages([message, m])
         return
 
     if Config.DATABASE_URI:
-        await m.edit("Mongo DB Found, Setting up recording title...") 
+        await m.edit("دیتابیس پیدا شد، درحال تنظیم ضبط عنوان..") 
         if title == "False":
-            await m.edit(f"Sucessfully removed custom recording title.")
+            await m.edit(f"ضبط عنوان دستی، با موفقیت حذف شد")
             Config.RECORDING_TITLE=False
             await sync_to_db()
             await delete_messages([message, m])           
@@ -61,29 +61,29 @@ async def recording_title(bot, message):
         else:
             Config.RECORDING_TITLE=title
             await sync_to_db()
-            await m.edit(f"Succesfully changed recording title to {title}")
+            await m.edit(f"عنوان ضبط تغییر پیدا کرد ب {title}")
             await delete_messages([message, m])
             return
     else:
         if not Config.HEROKU_APP:
             buttons = [[InlineKeyboardButton('Heroku API_KEY', url='https://dashboard.heroku.com/account/applications/authorizations/new'), InlineKeyboardButton('🗑 Close', callback_data='close'),]]
             await m.edit(
-                text="No heroku app found, this command needs the following heroku vars to be set.\n\n1. <code>HEROKU_API_KEY</code>: Your heroku account api key.\n2. <code>HEROKU_APP_NAME</code>: Your heroku app name.", 
+                text="هیچ برنامه ی هیروکویی پیدا نشد، این دستور نیاز به این مقادیر داره\n\n1. <code>HEROKU_API_KEY</code>: ای پی عای اکه هیروکو.\n2. <code>HEROKU_APP_NAME</code>: نام برنامه ی هیروکو", 
                 reply_markup=InlineKeyboardMarkup(buttons)) 
             await delete_messages([message])
             return     
         config = Config.HEROKU_APP.config()
         if title == "False":
             if "RECORDING_TITLE" in config:
-                await m.edit(f"Sucessfully removed custom recording title. Now restarting..")
+                await m.edit(f"ضبط عنوان دستی، با موفقیت حذف شد، درحال ریستارت..")
                 await delete_messages([message])
                 del config["RECORDING_TITLE"]                
                 config["RECORDING_TITLE"] = None
             else:
-                await m.edit(f"Its already default title, nothing was changed")
+                await m.edit(f"درحال حاظر، ضبط، رو عنوان پیشفرض تنظیمه، چیزی تغییر نکرده")
                 Config.RECORDING_TITLE=False
                 await delete_messages([message, m])
         else:
-            await m.edit(f"Succesfully changed recording title to {title}, Now restarting")
+            await m.edit(f"عنوان ضبط تغییر پیدا کرد ب {title}, درحال ریستارت..")
             await delete_messages([message])
             config["RECORDING_TITLE"] = title
