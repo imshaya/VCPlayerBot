bd2fec54f60ff37ba26b1ec2089f77d58bab7dd8
impta <87300042+impta@users.noreply.github.com>
2022-05-15 20:17:57 +0430

    Update install_python39.sh

--- install_python39.sh
+++ install_python39.sh
@@ -1,2 +1,7 @@
 sudo apt-get -y purge python3.8 && sudo apt-get -y autoremove
-wget https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz && tar xzf Python-3.9.9 && cd Python-3.9.9 && sudo ./configure --enable-optimizations && sudo make altinstall
+sudo apt update && sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9
+python3.9 --version
+
+
