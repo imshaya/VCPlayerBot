c67f4b244b871bd5bcd15bf4708f3c7bd4fbd659
impta <87300042+impta@users.noreply.github.com>
2022-05-25 00:34:45 +0430

    Update export_import.py

--- plugins/export_import.py
+++ plugins/export_import.py
@@ -44,13 +44,13 @@ admin_filter=filters.create(is_admin)
 @Client.on_message(filters.command(["export", f"export@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
 async def export_play_list(client, message: Message):
     if not Config.playlist:
-        k=await message.reply_text("Playlist is Empty")
+        k=await message.reply_text("پلی لیست خالیه")
         await delete_messages([message, k])
         return
     file=f"{message.chat.id}_{message.message_id}.json"
     with open(file, 'w+') as outfile:
         json.dump(Config.playlist, outfile, indent=4)
-    await client.send_document(chat_id=message.chat.id, document=file, file_name="PlayList.json", caption=f"Playlist\n\nتعداد: <code>{len(Config.playlist)}</code>\n\nJoin [El-enLiL](https://t.me/elenlil)")
+    await client.send_document(chat_id=message.chat.id, document=file, file_name="PlayList.json", caption=f"Playlist\n\nتعداد: <code>{len(Config.playlist)}</code>\n\nخالق [El-enLiL](https://t.me/elenlil)")
     try:
         os.remove(file)
     except:
@@ -62,14 +62,14 @@ async def import_playlist(client, m: Message):
     with suppress(MessageIdInvalid, MessageNotModified):
         if m.reply_to_message is not None and m.reply_to_message.document:
             if m.reply_to_message.document.file_name != "PlayList.json":
-                k=await m.reply("Invalid PlayList file given. Export your current Playlist using /export.")
+                k=await m.reply("فایله پلی لیست نامعتبره")
                 await delete_messages([m, k])
                 return
             myplaylist=await m.reply_to_message.download()
-            status=await m.reply("Trying to get details from playlist.")
+            status=await m.reply("درحاله گرفتنه اطلاعاته پلی لیست")
             n=await import_play_list(myplaylist)
             if not n:
-                await status.edit("Errors Occured while importing playlist.")
+                await status.edit("وارد کردنه اطلاعات پلی لیست با مشکل مواجه شد")
                 await delete_messages([m, status])
                 return
             if Config.SHUFFLE:
@@ -85,5 +85,5 @@ async def import_playlist(client, m: Message):
             else:
                 await delete_messages([m, status])
         else:
-            k = await m.reply("No playList file given.")
+            k = await m.reply("هیچ فایله پلی لیستی بهم ندادی")
             await delete_messages([m, k])
