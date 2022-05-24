82ffb122d55e41f2aeef3c5b566727d21bf07bd2
impta <87300042+impta@users.noreply.github.com>
2022-05-25 01:23:43 +0430

    Update recorder.py

--- plugins/recorder.py
+++ plugins/recorder.py
@@ -37,23 +37,23 @@ admin_filter=filters.create(is_admin)
 
 @Client.on_message(filters.command(["record", f"record@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
 async def record_vc(bot, message):
-    await message.reply("Configure you VCPlayer Recording settings from hereㅤㅤ ㅤ", reply_markup=(await recorder_settings()))
+    await message.reply("تنظیمه تنظیماته ضبطه ربات از اینجاㅤㅤ ㅤ", reply_markup=(await recorder_settings()))
     await delete_messages([message])
 
 @Client.on_message(filters.command(["rtitle", f"rtitle@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
 async def recording_title(bot, message):
-    m=await message.reply("Checking..")
+    m=await message.reply("چک کردن..")
     if " " in message.text:
         cmd, title = message.text.split(" ", 1)
     else:
-        await m.edit("Give me a new title. Use /rtitle < Custom Title >\nUse <code>False</code> to revert to default title")
+        await m.edit("بهم با این دشتور تایتل جدید بده، /rtitle")
         await delete_messages([message, m])
         return
 
     if Config.DATABASE_URI:
-        await m.edit("Mongo DB Found, Setting up recording title...") 
+        await m.edit("دیتابیس پیدا شد، درحاله تنظیمه تایتله ضبط") 
         if title == "False":
-            await m.edit(f"Sucessfully removed custom recording title.")
+            await m.edit(f"با موفقیت تایتله دستی حذف شد")
             Config.RECORDING_TITLE=False
             await sync_to_db()
             await delete_messages([message, m])           
@@ -61,29 +61,29 @@ async def recording_title(bot, message):
         else:
             Config.RECORDING_TITLE=title
             await sync_to_db()
-            await m.edit(f"Succesfully changed recording title to {title}")
+            await m.edit(f"با موفقیت تایتله ضبط تغییر پیدا کرد به {title}")
             await delete_messages([message, m])
             return
     else:
         if not Config.HEROKU_APP:
-            buttons = [[InlineKeyboardButton('Heroku API_KEY', url='https://dashboard.heroku.com/account/applications/authorizations/new'), InlineKeyboardButton('🗑 Close', callback_data='close'),]]
+            buttons = [[InlineKeyboardButton('Heroku API_KEY', url='https://dashboard.heroku.com/account/applications/authorizations/new'), InlineKeyboardButton('🗑 بستن', callback_data='close'),]]
             await m.edit(
-                text="No heroku app found, this command needs the following heroku vars to be set.\n\n1. <code>HEROKU_API_KEY</code>: Your heroku account api key.\n2. <code>HEROKU_APP_NAME</code>: Your heroku app name.", 
+                text="هیچ اپه هیروکویی پیدا نشد\n\n1. <code>HEROKU_API_KEY</code>: کلید ای پی عای اکانته هیروکو.\n2. <code>HEROKU_APP_NAME</code>: نام برنامه ی هیروکو", 
                 reply_markup=InlineKeyboardMarkup(buttons)) 
             await delete_messages([message])
             return     
         config = Config.HEROKU_APP.config()
         if title == "False":
             if "RECORDING_TITLE" in config:
-                await m.edit(f"Sucessfully removed custom recording title. Now restarting..")
+                await m.edit(f"با موفقیت تایتله ضبط حذف شد، درحاله ریست..")
                 await delete_messages([message])
                 del config["RECORDING_TITLE"]                
                 config["RECORDING_TITLE"] = None
             else:
-                await m.edit(f"Its already default title, nothing was changed")
+                await m.edit(f"درحاله حاظر تایتل تنظیمه رو پیشفرض، چیزی تغییر نکرد!")
                 Config.RECORDING_TITLE=False
                 await delete_messages([message, m])
         else:
-            await m.edit(f"Succesfully changed recording title to {title}, Now restarting")
+            await m.edit(f"با موفقیت تاتیل تغییر پیدا کرد به {title}, درحاله ریست..")
             await delete_messages([message])
             config["RECORDING_TITLE"] = title
